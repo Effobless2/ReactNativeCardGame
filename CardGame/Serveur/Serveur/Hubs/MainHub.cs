fa485446a2fd4fbe5ad264acb6aba4a888cf593e
Deboffle Maxime <effobless@hotmail.fr>
2018-04-27 18:15:19 +0200

    Mise en place MainHub coté serveur

---
+++ CardGame/Serveur/Serveur/Hubs/MainHub.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.SignalR;
+
+namespace Serveur.Hubs
+{
+    public class MainHub : Hub
+    {
+
+        public static string Path = "/mainhub";
+
+        public override async Task OnConnectedAsync()
+        {
+            Console.WriteLine("Passage dans Connection");
+            await Clients.All.SendAsync("Connect", Context.User.Identity.Name, "joined");
+            await base.OnConnectedAsync();
+        }
+
+        public override async Task OnDisconnectedAsync(Exception ex)
+        {
+            Console.WriteLine("Passage dans Déconnection");
+            await Clients.All.SendAsync("Disconnected", Context.User.Identity.Name, "left");
+        }
+
+        public async Task Send(string message)
+        {
+            Console.WriteLine("Passage dans Send");
+            await Clients.All.SendAsync("Action", Context.User.Identity.Name, message);
+        }
+    }
+}
