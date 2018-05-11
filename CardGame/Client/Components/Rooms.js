81e4afc6880fb2a09569fd511ee88a98c681affe
Deboffle Maxime <effobless@hotmail.fr>
2018-05-11 15:33:32 +0200

    Listview de Rooms ne se vide plus si modifs mais ne se met pas à jour pour autant

--- CardGame/Client/Components/Rooms.js
+++ CardGame/Client/Components/Rooms.js
@@ -7,6 +7,15 @@ export class Rooms extends React.Component{
         super(props);
         console.log("construct room")
         this.connection = props.screenProps
+        const datas = [];
+        console.log(this.connection.cardGame.Rooms)
+        for (var room of this.connection.cardGame.Rooms.values()){
+            datas.push(room);
+            console.log("a")
+        }
+        this.state = {
+            datas: datas
+        }
     
     }
 
@@ -15,19 +24,13 @@ export class Rooms extends React.Component{
     }
 
     render(){
-        const datas = [];
-        console.log(this.connection.cardGame.Rooms)
-        for (var room of this.connection.cardGame.Rooms.values()){
-            datas.push(room);
-            console.log("a")
-        }
         
         const ds = new ListView.DataSource({rowHasChanged: (r1,r2) => r1 != r2});
         return (
             <View style = {Styles.container}>
                 <Text>{this.connection.cardGame.currentUser.userName}</Text>
                 <ListView
-                    dataSource={ds.cloneWithRows(datas)}
+                    dataSource={ds.cloneWithRows(this.state.datas)}
                     renderRow = {(rowData, i, j) => <Text>{j}, {rowData.roomId}</Text>}
                 />
             </View>
