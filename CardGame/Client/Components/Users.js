f4a0d8d7586631fc4e49399757b5a18b3fdd4551
Deboffle Maxime <effobless@hotmail.fr>
2018-05-15 22:22:03 +0200

    Affichage tous les users connectés à la connection ok

--- CardGame/Client/Components/Users.js
+++ CardGame/Client/Components/Users.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, ScrollView } from 'react-native';
 
 import { Styles } from '../Styles';
 
@@ -10,13 +10,28 @@ class Users extends React.Component{
         super(props);
     }
 
+    renderUserList(){
+        let counter = 0;
+        return Array.from(this.props.users).map(([id, value]) =>{
+            counter++;
+            if (id != this.props.cardGame.currentUser.userId){
+                return (
+                    <Text key={counter}>{value.userName}</Text>
+                );
+            }
+        });
+    }
+
     render(){
         return (
             <View style = {Styles.container}>
-                <Text>Users</Text>
+                <ScrollView style = {Styles.scroll} contentContainerStyle={Styles.scrollViewRoomItem}>
+                    {this.renderUserList()}
+                </ScrollView>
             </View>
         );
     }
 }
 
-export default Users;
+const mapStateToProps = ({cardGame}) => ({cardGame :cardGame.cardGame, connected : cardGame.connected, users: cardGame.cardGame.Users})
+export default connect(mapStateToProps)(Users)
