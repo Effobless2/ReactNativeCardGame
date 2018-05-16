2e81af6c6ba67dfa412dab10563af000922866c8
Deboffle Maxime <effobless@hotmail.fr>
2018-05-16 10:39:50 +0200

    Button new Room created

--- CardGame/Client/screens/Home.js
+++ CardGame/Client/screens/Home.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { StyleSheet, Text, View, Button } from 'react-native';
+import { StyleSheet, Text, View, Button, TouchableOpacity } from 'react-native';
 import { Styles } from '../Styles';
 import { Tabs } from '../components/Navigator';
 import { connect } from 'react-redux';
@@ -11,11 +11,19 @@ class Home extends React.Component{
         //console.log(this.props)
     }
 
+    creatingARoom(){}
+
     render(){
         return (
             <View style = {{flex: 1}}>
-                <View style = {{height: 50, paddingTop: 20, marginLeft: 10}}>
+                <View style = {{height: 50, paddingTop: 20, marginLeft: 10, marginRight: 10, flexDirection:"row", justifyContent:"space-between", alignItems:"center"}}>
                     <Text style = {{fontSize:20}}>{this.props.cardGame.currentUser.userName}</Text>
+                    <TouchableOpacity
+                        onPress = {() => this.creatingARoom()}>
+                        <Text style = {{fontSize: 20}}>
+                            New Room
+                        </Text>
+                    </TouchableOpacity>
                 </View>
                 <Tabs/>
             </View>
