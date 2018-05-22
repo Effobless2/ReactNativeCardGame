4f3fa8d88a4d2648125a3f85b5b09b1629662a68
Deboffle Maxime <effobless@hotmail.fr>
2018-05-22 11:50:54 +0200

    Composant asPublic realisé

--- CardGame/Client/Components/AsPublicComponent.js
+++ CardGame/Client/Components/AsPublicComponent.js
@@ -1,13 +1,30 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, ScrollView } from 'react-native';
 import {Styles} from '../Styles';
 import { connect } from 'react-redux';
+import CurrentRoom from './JoinedRoomItem';
 
 class AsPublicComponent extends React.Component{
+
+    renderRoomList(){
+        let counter = 0;
+        console.log(this.props.cardGame.roomsAsPlayer.length)
+        return this.props.cardGame.roomsAsPublic.map(element => {
+            counter ++;
+            console.log("renderROomList")
+            return (
+                <CurrentRoom key={element} room ={this.props.cardGame.getRoom(element)}/>
+            );
+        });
+    }
+
     render(){
         return (
             <View style={Styles.usersRoomComponents}>
                 <Text>AsPublic</Text>
+                <ScrollView style ={Styles.scroll} contentContainerStyle={Styles.scrollViewRoomItem}>
+                    {this.renderRoomList()}
+                </ScrollView>
             </View>
         );
     }
