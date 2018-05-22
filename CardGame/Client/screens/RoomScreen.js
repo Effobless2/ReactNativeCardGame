f703d06ad0a2259a5ab947ed0ec0605d634a0746
Deboffle Maxime <effobless@hotmail.fr>
2018-05-22 17:40:46 +0200

    picker mit en place

--- CardGame/Client/screens/RoomScreen.js
+++ CardGame/Client/screens/RoomScreen.js
@@ -1,20 +1,43 @@
 import React from 'react';
-import { View, Text, TouchableOpacity } from 'react-native';
+import { View, Text, TouchableOpacity, Picker } from 'react-native';
 import { connect } from 'react-redux';
 import { Styles } from '../Styles';
 import { selectRoom } from '../actions';
 
 class RoomScreen extends React.Component{
+
+    renderRoomList(){
+        let counter = 0;
+        return this.props.cardGame.roomsAsPlayer.concat(this.props.cardGame.roomsAsPublic).map((roomId) =>{
+            counter++;
+            return (
+                <Picker.Item 
+                    label= {'room '+counter}
+                    value = {roomId}
+                    key = {counter} 
+                />
+            );
+        });
+    }
+
     render(){
         return (
             <View style = {{flex: 1}}>
                 <View style = {{height: 50, paddingTop: 20, marginLeft: 10, marginRight: 10, flexDirection:"row", justifyContent:"space-between", alignItems:"center"}}>
                     <TouchableOpacity
+                    style = {{flex: 1}}
                         onPress = {() => this.props.selectRoom(null)}>
                         <Text style = {{fontSize: 20}}>
                             Go Back
                         </Text>
                     </TouchableOpacity>
+                    <Picker
+                    style= {{flex: 1}}
+                        selectedValue = {this.props.selectedRoom}
+                        onValueChange = {(itemValue, itemIndex) => {this.props.selectRoom(itemValue)}}>
+                        {this.renderRoomList()}
+                    </Picker>
+                    
                 </View>
                 <View style = {Styles.container}>
                     <Text>
