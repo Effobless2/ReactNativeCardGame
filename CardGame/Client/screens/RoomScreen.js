1ebaae4e78e1bdf791b263c7b930c3f6299ebbf4
Deboffle Maxime <effobless@hotmail.fr>
2018-05-22 16:58:03 +0200

    goback button ok

--- CardGame/Client/screens/RoomScreen.js
+++ CardGame/Client/screens/RoomScreen.js
@@ -1,14 +1,20 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, TouchableOpacity } from 'react-native';
 import { connect } from 'react-redux';
 import { Styles } from '../Styles';
+import { selectRoom } from '../actions';
 
 class RoomScreen extends React.Component{
     render(){
         return (
             <View style = {{flex: 1}}>
                 <View style = {{height: 50, paddingTop: 20, marginLeft: 10, marginRight: 10, flexDirection:"row", justifyContent:"space-between", alignItems:"center"}}>
-                    <Text>HEADER</Text>
+                    <TouchableOpacity
+                        onPress = {() => this.props.selectRoom(null)}>
+                        <Text style = {{fontSize: 20}}>
+                            GoBack
+                        </Text>
+                    </TouchableOpacity>
                 </View>
                 <View style = {Styles.container}>
                     <Text>
@@ -21,4 +27,4 @@ class RoomScreen extends React.Component{
 }
 
 const mapStateToProps = ({cardGame, selectedRoom}) => ({cardGame :cardGame.cardGame, connected : cardGame.connected, selectedRoom: cardGame.selectedRoom})
-export default connect(mapStateToProps)(RoomScreen);
+export default connect(mapStateToProps, { selectRoom })(RoomScreen);
