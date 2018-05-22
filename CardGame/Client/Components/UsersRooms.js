4bf7d3b227cb67c008132bb575cb51739090b323
Deboffle Maxime <effobless@hotmail.fr>
2018-05-22 10:15:38 +0200

    UsersRooms connecté avec redux

--- CardGame/Client/Components/UsersRooms.js
+++ CardGame/Client/Components/UsersRooms.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { View, Text } from 'react-native';
-import {Styles} from '../Styles';
+import { Styles } from '../Styles';
+import { connect } from 'react-redux';
 
 class UsersRooms extends React.Component{
     render(){
@@ -14,4 +15,5 @@ class UsersRooms extends React.Component{
     }
 }
 
-export default UsersRooms;
+const mapStateToProps = ({cardGame}) => ({cardGame :cardGame.cardGame, connected : cardGame.connected, users: cardGame.cardGame.Users, cpt: cardGame.cpt})
+export default connect(mapStateToProps)(UsersRooms)
